b4b2f06188bc20dbe6b7266861ea6b443f6b66d0
정호윤 <126047439+Jhoyoon@users.noreply.github.com>
2025-10-07 02:41:32 +0900

    mongoose 이것저것

--- src/Controller/firstPageController.js
+++ src/Controller/firstPageController.js
@@ -1,7 +1,8 @@
 import Video from '../models/Video.js';
 
 export const firstPageController = async (req, res,next) => {
-    const result = await Video.find({});
+    console.log('firstPageController run');
+    const result = (await Video.find({})).sort({createdDate : "asc"}); // 이런식으로 정렬도 가능
     console.log('result :: '+result);
     const pateTitle = {pateTitle : "타이틀임",condition : false,videos : []};
     res.render("home",pateTitle);
@@ -9,4 +10,42 @@ export const firstPageController = async (req, res,next) => {
 export const homeController = (req, res,next) => {
     // 여기서 파라미터를 view에 전달하면 되는구나?
     res.send("<h1>home</h1>");
+}
+export const getUpload = (req, res,next) => {
+    // 여기서 파라미터를 view에 전달하면 되는구나?
+    const pateTitle = {pateTitle : "upload",condition : false,videos : []};
+    res.render("upload",pateTitle);
+}
+export const postUpload = async (req, res,next) => {
+    console.log('postUpload run');
+    const { title, description } = req.body; // 아래 코드랑 동일
+    // console.log('req.body.title :: '+req.body.title);
+    // console.log('req.body.description :: '+req.body.description);
+    // 검색해서 찾기
+    // await Video.findById();
+    // 검색해서 찾고 바로 update 하기
+    // await Video.findByIdAndUpdate(id : id,{title : tile, something...})
+    // 삭제해버려~
+    // await Video.findByIdAndDelete(id : id);
+    // moongose에도 트리거 개념이 존재한다!!
+    // req.query url 쿼리 파라미터 가져올수 있음
+    await Video.find({
+        title : {
+            $regex : new RegExp(keyword,"i") // 정규식으로 검색 가능. mongodb 문서에 쿼리 옵션들 더 많이 있음
+        }
+    })
+    const video = new Video({ // 아래거랑 동
+        title,
+        description : Video.myStatic('something')
+
+    });
+    // Video.create({
+    //     title,
+    //     description,
+    //     createdDate : Date.now()
+    // });
+    const result = await video.save();
+    console.log('result :: '+result);
+    // const pateTitle = {pateTitle : "upload",condition : false,videos : []};
+    res.redirect('/');
 }
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,14 +1,24 @@
 import mongoose from "mongoose"
 
 const videoSchema = new mongoose.Schema({
-    title : String,
-    createdDate : Date,
+    title : {type : String, required : true,trim : true,maxLength : 255,minLength : 1},
+    description : {type : String, maxLength : 255,minLength : 1},
+    createdDate : {type : Date, required : true, default : Date.now}, // moongose가 알아서 now() 함수로 변환시켜줌
     hashtags : [{type : String}],
     meta : {
         views : Number,
         rating : Number
     }
 });
-
+// 트리거 개념이 존재한다!!
+// 근데 또 모든 api에서 작동하는건 아님...까다롭네.
+// 이거는 findByIdAndUpate api에서는 작동을 하지 않음
+videoSchema.pre('save',async function(){ // pre = before
+    console.log('this :: '+this); 
+    this.title = 'before trigger~'; // 이런식으로 데이터 수정 가능
+});
+videoSchema.static('myStatic',function(something){
+    return 'something output';
+});
 const Video = mongoose.model("Video",videoSchema);
 export default Video;
